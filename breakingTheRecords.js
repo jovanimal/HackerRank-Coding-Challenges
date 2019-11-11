213a58ca92cd869acfa5db3655ea5ae9a0ee1b64
Jovan Goh <54385240+jovanimal@users.noreply.github.com>
2019-11-11 11:51:04 +0800

    add breakingTheRecords.js

---
+++ breakingTheRecords.js
@@ -0,0 +1,16 @@
+function breakingRecords(scores) {
+  let breakHigh = 0;
+  let breakLow = 0;
+  for (let i = 0; i < scores.length; i++) {
+    let highStop = Math.max(...scores.slice(0,i+1));
+    let lowStop = Math.min(...scores.slice(0,i+1));
+    if (scores[i+1] > highStop) breakHigh ++;
+    if (scores[i+1] < lowStop) breakLow ++;
+  }
+
+  return [breakHigh, breakLow];
+}
+
+breakingRecords([3,4,21,36,10,28,35,5,24,42])
+breakingRecords([10,5,20,20,4,5,2,25,1])
+breakingRecords([100,45,41,60,17,41,45,43,100,40,89,92,34,6,64,7,37,81,32,50])
