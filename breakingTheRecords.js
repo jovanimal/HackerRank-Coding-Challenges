8224e99f32a0bd2c42b7234c788f3374577c0adb
Jovan Goh <54385240+jovanimal@users.noreply.github.com>
2019-11-12 09:50:03 +0800

    add breakingTheRecords

--- breakingTheRecords.js
+++ breakingTheRecords.js
@@ -12,5 +12,4 @@ function breakingRecords(scores) {
 }
 
 breakingRecords([3,4,21,36,10,28,35,5,24,42])
-breakingRecords([10,5,20,20,4,5,2,25,1])
 breakingRecords([100,45,41,60,17,41,45,43,100,40,89,92,34,6,64,7,37,81,32,50])
