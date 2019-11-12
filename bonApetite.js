e7186df68f9aaf66c8e1ebcb9fd34737d6718500
Jovan Goh <54385240+jovanimal@users.noreply.github.com>
2019-11-12 09:32:00 +0800

    add bon apetit

---
+++ bonApetite.js
@@ -0,0 +1,8 @@
+function bonAppetit(bill, k, b) {
+  let totalBill = bill.reduce((a,c) => a + c);
+  let lessEater = (totalBill - bill[k]) / 2;
+  if (b === lessEater) console.log("Bon Appetit");
+  if (b > lessEater) console.log(b-lessEater);
+}
+
+bonAppetit([3,10,2,9],1,7)
